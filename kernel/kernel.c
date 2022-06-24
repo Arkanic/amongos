05fef99fd3aa602643cd3b7ec472534fabdd466e
Arkanic <silverfishasaurus@gmail.com>
2022-06-24 23:07:05 +0000

    ports

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,17 @@
-void dummy(void) {}
+#include "../drivers/ports.h"
 
 void main(void) {
-    char *video_memory = (char *)0xb8000;
-    *video_memory = 'X';
+    port_byte_out(0x3d4, 14);
+
+    int position = port_byte_in(0x3d5);
+    position = position << 8;
+
+    port_byte_out(0x3d4, 15);
+    position += port_byte_in(0x3d5);
+
+    int offset_from_vga = position * 2;
+
+    char *vga = (char *)0xb8000;
+    vga[offset_from_vga] = 'X';
+    vga[offset_from_vga+1] = 0x0f;
 }

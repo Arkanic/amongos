2fb01ed9b2eeda6391857a4ea8878d2b25a491fc
Arkanic <silverfishasaurus@gmail.com>
2022-06-24 23:50:12 +0000

    util

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,8 +1,8 @@
 #include "screen.h"
 #include "ports.h"
+#include "../kernel/util.h"
 
-// private functions
-int get_cursor_offset(void);
+int get_cursor_offset();
 void set_cursor_offset(int offset);
 int print_char(char c, int col, int row, char attr);
 int get_offset(int col, int row);
@@ -11,18 +11,17 @@ int get_offset_col(int offset);
 
 void kprint_at(char *message, int col, int row) {
     int offset;
-    if(col >= 0 && row >= 0) {
+    if (col >= 0 && row >= 0)
         offset = get_offset(col, row);
-    } else {
+    else {
         offset = get_cursor_offset();
         row = get_offset_row(offset);
         col = get_offset_col(offset);
     }
 
     int i = 0;
-    while(message[i] != 0) {
+    while (message[i] != 0) {
         offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
-
         row = get_offset_row(offset);
         col = get_offset_col(offset);
     }
@@ -33,8 +32,8 @@ void kprint(char *message) {
 }
 
 int print_char(char c, int col, int row, char attr) {
-    unsigned char *vidmem = (unsigned char *)VIDEO_ADDRESS;
-    if(!attr) attr = WHITE_ON_BLACK;
+    unsigned char *vidmem = (unsigned char*) VIDEO_ADDRESS;
+    if (!attr) attr = WHITE_ON_BLACK;
 
     if (col >= MAX_COLS || row >= MAX_ROWS) {
         vidmem[2*(MAX_COLS)*(MAX_ROWS)-2] = 'E';
@@ -54,11 +53,24 @@ int print_char(char c, int col, int row, char attr) {
         vidmem[offset+1] = attr;
         offset += 2;
     }
+
+    if (offset >= MAX_ROWS * MAX_COLS * 2) {
+        int i;
+        for (i = 1; i < MAX_ROWS; i++) 
+            memory_copy(get_offset(0, i) + VIDEO_ADDRESS,
+                        get_offset(0, i-1) + VIDEO_ADDRESS,
+                        MAX_COLS * 2);
+        char *last_line = get_offset(0, MAX_ROWS-1) + VIDEO_ADDRESS;
+        for (i = 0; i < MAX_COLS * 2; i++) last_line[i] = 0;
+
+        offset -= 2 * MAX_COLS;
+    }
+
     set_cursor_offset(offset);
     return offset;
 }
 
-int get_cursor_offset(void) {
+int get_cursor_offset() {
     port_byte_out(REG_SCREEN_CTRL, 14);
     int offset = port_byte_in(REG_SCREEN_DATA) << 8;
     port_byte_out(REG_SCREEN_CTRL, 15);
@@ -74,10 +86,10 @@ void set_cursor_offset(int offset) {
     port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
 }
 
-void clrscr(void) {
+void clear_screen() {
     int screen_size = MAX_COLS * MAX_ROWS;
     int i;
-    char *screen = (char *)VIDEO_ADDRESS;
+    char *screen = VIDEO_ADDRESS;
 
     for (i = 0; i < screen_size; i++) {
         screen[i*2] = ' ';
@@ -85,6 +97,7 @@ void clrscr(void) {
     }
     set_cursor_offset(get_offset(0, 0));
 }
+
 
 int get_offset(int col, int row) { return 2 * (row * MAX_COLS + col); }
 int get_offset_row(int offset) { return offset / (2 * MAX_COLS); }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,15 @@
 #include "../drivers/screen.h"
+#include "util.h"
 
 void main(void) {
     clrscr();
-    kprint_at("X", 1, 6);
-    kprint_at("This text spans multiple lines", 75, 10);
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line\nbreak");
-    kprint_at("What happens when we run out of space?", 45, 24);
+
+    for(int i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i);
+    }
+
+    kprint_at("This text forces the kernel to scroll, row 0 will disappear", 60, 24);
+    kprint("and with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
